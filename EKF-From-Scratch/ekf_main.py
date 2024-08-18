d48e2b03363030eeb58d9a70c5109f7a2fb2634b
IremOztimur <iremoztimur07@gmail.com>
2024-08-18 16:55:50 +0300

    [ADDED] Jacobian Matrix and Covariance Matrix implemented

--- EKF-From-Scratch/ekf_main.py
+++ EKF-From-Scratch/ekf_main.py
@@ -23,6 +23,9 @@ import pygame
 n_state = 3 # state dimension of the robot (x, y, theta)
 n_landmarks = 1 # DEFAULT: 1 landmark
 
+# ---› Noise parameters
+R = np.diag([0.002,0.002,0.0005])
+
 # ---> EKF Estimation Variables
 mu = np.zeros((n_state + 2*n_landmarks, 1)) # state estimate
 sigma = np.zeros((n_state + 2*n_landmarks, n_state + 2*n_landmarks)) # covariance matrix
@@ -33,12 +36,19 @@ Fx = np.block([[np.eye(n_state), np.zeros((n_state, 2*n_landmarks))]]) # state t
 def prediction_update(mu, sigma, u, dt):
     rx, ry, theta = mu[0], mu[1], mu[2]
     v, w = u[0], u[1]
-    # update state estimate mu with model
+    # Ipdate state estimate mu with model
     state_model_mat = np.zeros((n_state, 1))
     state_model_mat[0] = -(v/w)*np.sin(theta) + (v/w)*np.sin(theta + w*dt) if np.abs(w) >= 0.01 else v*dt*np.cos(theta)
     state_model_mat[1] = (v/w)*np.cos(theta) - (v/w)*np.cos(theta + w*dt) if np.abs(w) >= 0.01 else v*dt*np.sin(theta)
     state_model_mat[2] = w*dt
     mu += np.transpose(Fx).dot(state_model_mat)
+    # Update state uncertainty with model + noise
+    state_jacobian_mat = np.zeros((n_state, 3))
+    state_jacobian_mat[0][2] = -(v/w)*np.cos(theta) + (v/w)*np.cos(theta + w*dt) if np.abs(w) >= 0.01 else -v*dt*np.sin(theta)
+    state_jacobian_mat[1][2] = -(v/w)*np.sin(theta) + (v/w)*np.sin(theta + w*dt) if np.abs(w) >= 0.01 else v*dt*np.cos(theta)
+    I = np.eye(sigma.shape[0])
+    G = I + np.transpose(Fx).dot(state_jacobian_mat).dot(Fx)
+    sigma = G.dot(sigma).dot(np.transpose(G)) + np.transpose(Fx).dot(R).dot(Fx)
     return mu, sigma
 
 def measurement_update(mu, sigma):
